6bb185a9a47bd1c93bed40d75b2fd5feb937bb39
J K <kj688895@gmail.com>
2020-09-26 23:19:42 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,20 +1,20 @@
 require 'pry'
 class Meal
-  attr_accessor :waiter, :customer, :total, :tip 
+  attr_accessor :waiter, :customer, :total, :tip
 
   @@all = []
 
   def initialize(waiter, customer, total, tip = 0)
-    @waiter = waiter 
-    @customer = customer 
-    @total = total 
-    @tip = tip 
-    @@all  << self 
-  end 
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all  << self
+  end
 
-  def self.all 
-    @@all 
-  end 
+  def self.all
+    @@all
+  end
 
   
 
